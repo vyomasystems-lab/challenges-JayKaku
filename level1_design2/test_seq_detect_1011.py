23bb2f8fee183a036d35415284ed3a645d8fa26e
Jay Kaku <jay8kaku@gmail.com>
2022-07-28 01:16:31 +0530

    level1-design2: 1 test added

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -1,7 +1,7 @@
 # See LICENSE.vyoma for details
 
 # SPDX-License-Identifier: CC0-1.0
-
+import re
 import os
 import random
 from pathlib import Path
@@ -27,11 +27,17 @@ async def test_seq_bug1(dut):
     cocotb.log.info('#### CTB: Develop your test here! ######')
 
     inp = [1,1,1,0,1,1,1,1]
+    string=""
 
     for i in inp:
+        string+= str(i)
         dut.inp_bit.value = i
         await Timer(10, units='us')
+
+        check = re.findall("1011",string)
+
         dut._log.info(f'clk={int(dut.clk.value)} rstn={int(dut.reset.value)} inp_bit={int(dut.inp_bit.value)} seq_seen={int(dut.seq_seen.value):02}')
+        assert dut.seq_seen.value == check, "Error"
 
     
 
